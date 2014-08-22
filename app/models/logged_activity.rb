87ff07ab83d75c22b1fc194cc9b105a63edc8e09
Katie Delfin <katiedelfin@gmail.com>
2014-08-22 18:31:51 -0400

    Pretify intensity model a bit.

--- app/models/logged_activity.rb
+++ app/models/logged_activity.rb
@@ -1,23 +1,22 @@
 class LoggedActivity < ActiveRecord::Base
 
+  enum intensity: { :low => 0, :medium => 50, :high => 100 }
+
+  INTENSITY_MULTIPLIER = {
+    low:    1.1,
+    medium: 1.2,
+    high:   1.3
+  }.freeze
+
   belongs_to :user
   belongs_to :activity
 
   before_save :calculate_points
 
-  enum intensity: [ :low, :medium, :high ]
-
   private
 
   def calculate_points
-    case intensity
-    when :low
-      points = duration * 1.1
-    when :medium
-      points = duration * 1.2
-    when :high
-      points = duration * 1.3
-    end
+    self.points = duration * INTENSITY_MULTIPLIER[intensity.to_sym]
   end
 
 end
