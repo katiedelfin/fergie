02f44b69ab5ff839d9683bf510f6186ca874328e
Katie Delfin <katiedelfin@gmail.com>
2014-08-22 15:15:30 -0400

    Add base configuration for google oauth.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "565046678185-gbp45jtkt9vlmrq247v0fpuu71bve0dl.apps.googleusercontent.com", "OBfh0TjYt9rI30taTpf25yq3"
+end
