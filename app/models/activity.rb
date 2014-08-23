5a26bf8bca2d53ea8a46489368ace0314b4531fa
Katie Delfin <katiedelfin@gmail.com>
2014-08-22 22:32:44 -0400

    Validate uniqueness of activity name

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,10 +2,18 @@ class Activity < ActiveRecord::Base
 
   has_many :logged_activities, dependent: :restrict_with_error
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  before_validation :titlecase_name
 
   def to_s
     name
+  end
+
+  private
+
+  def titlecase_name
+    self.name = self.name.titlecase
   end
 
 end
