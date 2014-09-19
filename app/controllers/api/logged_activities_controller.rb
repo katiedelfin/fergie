d70c1f966a62270f756892cf90020384a3fc5291
Katie Delfin <katiedelfin@gmail.com>
2014-09-19 14:07:26 -0400

    Include activity information in logged_activities api

--- app/controllers/api/logged_activities_controller.rb
+++ app/controllers/api/logged_activities_controller.rb
@@ -1,19 +1,19 @@
 module Api
   class LoggedActivitiesController < ApplicationController
     def index
-      logged_activities = LoggedActivity.all
-      render json: logged_activities, status: 200
+      logged_activities = LoggedActivity.all.includes(:activity)
+      render json: logged_activities.to_json(include: :activity), status: 200
     end
 
     def create
       logged_activity = LoggedActivity.create(logged_activity_params)
-      render json: logged_activity, status: 200
+      render json: logged_activity.to_json(include: :activity), status: 200
     end
 
     def destroy
-      logged_activity = LoggedActivity.find params[:id]
+      logged_activity = LoggedActivity.find(params[:id]).includes(:activity)
       logged_activity.destroy
-      render json: logged_activity, status: 200
+      render json: logged_activity.to_json(include: :activity), status: 200
     end
 
     private
