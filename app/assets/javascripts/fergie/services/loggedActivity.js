c399482045db131514d1c71a384ce91b4e7ac790
Russell Centanni <russell.centanni@gmail.com>
2014-09-19 13:19:01 -0400

    Fixed indentation.

--- app/assets/javascripts/fergie/services/loggedActivity.js
+++ app/assets/javascripts/fergie/services/loggedActivity.js
@@ -1,6 +1,6 @@
 angular.module('fergie.services').factory('LoggedActivity', ['railsResourceFactory', function (railsResourceFactory) {
-    return railsResourceFactory({
-        url: '/api/logged_activities',
-        name: 'logged_activity'
-    });
+  return railsResourceFactory({
+    url: '/api/logged_activities',
+    name: 'logged_activity'
+  });
 }]);
